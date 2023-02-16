d0c0b784adba47b7e25139f7ae8b74511a3394b2
LidorLazar <lidorlazar33@gmail.com>
2023-02-17 01:27:35 +0200

    change the file

--- backend/my_project/urls.py
+++ backend/my_project/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.url import staticfile_urlpatterns
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -27,6 +27,6 @@ urlpatterns = [
 
 ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += staticfile_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
