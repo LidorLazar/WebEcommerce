c7b4c3fa20265dd66a75f24d7ab726e8fa3079a3
LidorLazar <lidorlazar33@gmail.com>
2023-02-18 01:44:38 +0200

    finish project

--- backend/base/models.py
+++ backend/base/models.py
@@ -84,7 +84,7 @@ class Order(models.Model):
     city = models.CharField(max_length = 100, null = False, blank = True)
     country = models.CharField(max_length = 50, null = False, blank = True)
     zip_code = models.CharField(max_length = 15, null = False, blank = False)
-    price =models.IntegerField(null = True, blank = True)
+    total =models.IntegerField(null = True, blank = True)
 
     def __str__(self):
         return str(self.id)
